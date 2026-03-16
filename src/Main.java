9b696d22bbf67d2db985429757cf9bef877cebd7
Doruk Uslu <dorukuslu@172.23.81.136.wi-fi.pjwstk.edu.pl.pjwstk.edu.pl>
2026-03-16 09:50:11 +0100

    Add helper information about the application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("PGO Git exercise started.");
+        System.out.println("Repository prepared for branch and merge practice.");
     }
 }
