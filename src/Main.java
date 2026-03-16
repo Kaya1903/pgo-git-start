7ca3857c8c3ddd456d65386befd796edb4925f12
Doruk Uslu <dorukuslu@172.23.81.136.wi-fi.pjwstk.edu.pl.pjwstk.edu.pl>
2026-03-16 09:55:14 +0100

    Improve console output message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("PGO Git exercise started.");
         System.out.println("Repository prepared for branch and merge practice.");
+	System.out.println("This project demonstrates Git workflow.");
     }
 }
